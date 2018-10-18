95d50adab00d833cdb91bd7fb92341f0b5b6300f
bgatram <bgatram@gmail.com>
2018-10-18 17:42:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import TasksPage from './components/TasksPage';
+import { createTask, editTask, fetchTasks } from './actions';
 
 class App extends Component {
+
+  componentDidMount() {
+      this.props.dispatch(fetchTasks());
+  }
+  onCreateTask = ({ title, description}) => {
+      this.props.dispatch(createTask({ title, description }));
+  }
+
+  onStatusChange = (id, status) => {
+      this.props.dispatch(editTask(id, { status }))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-content">
+        <TasksPage
+            tasks={this.props.tasks}
+            onCreateTask={this.onCreateTask}
+            onStatusChange={this.onStatusChange}
+        />
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+
+    return {
+      tasks: state.tasks
+    }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/Index.js
@@ -0,0 +1,47 @@
+
+export default function tasks(state = { tasks: [] }, action) {
+    switch (action.type) {
+        case 'CREATE_TASK': {
+            return {
+                tasks: state.tasks.concat(action.payload),
+            };
+        }
+        case 'EDIT_TASK': {
+            const { payload } = action;
+            return {
+                tasks: state.tasks.map(task => {
+                    if (task.id === payload.id) {
+                        return Object.assign({}, task, payload.params);
+                    }
+                    return task;
+                }),
+            };
+        }
+        case 'FETCH_TASKS_SUCCEEDED': {
+            return {
+                tasks: action.payload.tasks,
+            };
+        }
+        case 'CREATE_TASK_SUCCEEDED': {
+            return {
+                tasks: state.tasks.concat(action.payload.task),
+            };
+        }
+
+        case 'EDIT_TASK_SUCCEEDED': {
+            const { payload } = action;
+            return {
+                tasks: state.tasks.map(task => {
+                    if (task.id === payload.task.id) {
+                        return payload.task;
+                    }
+                    return task;
+                }),
+            };
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
